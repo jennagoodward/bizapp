6947337e9248dc7ecc12fe12060be8fd6b8a3e8b
jennagoodward <jenna.goodward@gmail.com>
2013-03-11 07:42:56 +0100

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bizapp::Application.config.secret_token = 'cf9a77370499746b494c775803d41de8c77f5e3dfe086661402aad85686b80225b85e0774e89f8166676aab7c72fe66fd8d688ede8de9e1306f0f3b2a9e6a7b2'
